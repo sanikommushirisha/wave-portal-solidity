31dc5f2e3e21b17b54eabcf542dad6f95ce5fa47
sanikommushirisha <sanikommushirisha@gmail.com>
2022-10-07 03:31:57 -0700

    learn solidity basics

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+// Import and configure dotenv
+require("dotenv").config();
+// This is a sample Hardhat task. To learn how to create your own go to
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      // This value will be replaced on runtime
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      url: process.env.PROD_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
